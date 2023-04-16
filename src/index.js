3c698f51f519062f2cbe456a9ff69793c2297e6e
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-04-16 17:23:21 +0100

    create

--- src/index.js
+++ src/index.js
@@ -3,49 +3,62 @@ import { fetchPictures } from './js/fetchPictures'
 import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
 
+
 const refs = {
     searchFormEl: document.querySelector('.search-form'),
     galleryEl: document.querySelector('.gallery'),
   loadMoreBtnEl: document.querySelector('.load-more'),
+  totalItemsEl:document.querySelector('.totalItemsInfo'),
 }
 
 let currentPage = null;
 const limOnPage = 40;
 let totalItems = null;
 let valueOfInput = null;
-refs.searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+ const options={
+   closeText: 'x',
+   captionsData: "alt",
+   captionDelay: 500,
+  }
+  let gallery = new SimpleLightbox('.gallery a', options);
 
+
+refs.searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+refs.loadMoreBtnEl.addEventListener('click', handleLoadMoreClick);
 
 function  handleSearchFormSubmit(event) {
   event.preventDefault();
-  refs.loadMoreBtnEl.classList.add('hidden');
+  addClassHidden();
   refs.galleryEl.innerHTML = '';
   currentPage = 1;
   valueOfInput = event.target.elements.searchQuery.value;
   getFetchPicture(valueOfInput);
+  event.target.elements.searchQuery.value=''
+}
+
+function addClassHidden() {
+  refs.totalItemsEl.classList.add('hidden')
+  refs.loadMoreBtnEl.classList.add('hidden');
 }
 
 async function getFetchPicture(valueOfInput) {
   const response = await fetchPictures(valueOfInput, currentPage, limOnPage);
- 
   totalItems = response?.data?.totalHits;
   const arrayOfItems = response?.data?.hits;
   if (arrayOfItems.length === 0) {
-        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.',{width: '400px', fontSize: '18px'});
     return
   }
-  
  buildMarkup(arrayOfItems);
 }
-
 
 function buildMarkup(arrayOfItems) {
   isListOfItemsFinished();
   addTotalHitsInfo(totalItems);
-    const markupString= arrayOfItems.reduce((acc, {webformatURL, largeImageURL, tags, likes, views,comments,downloads}) => {
-        return acc += `
+  const markupString = arrayOfItems.reduce((acc, { webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+    return acc += `
 <div class="photo-card">
-  <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" width="400px" /></a>
+  <a class="gallery-link" href="${largeImageURL}"><img class="gallery-img" src="${webformatURL}" alt="${tags}" loading="lazy" height="200px" width="280px"/></a>
   <div class="info">
     <p class="info-item">
       <b>Likes ${likes}</b>
@@ -62,35 +75,47 @@ function buildMarkup(arrayOfItems) {
   </div>
 </div>
         `
-    }, '')
-  refs.galleryEl.insertAdjacentHTML('beforeend', markupString);
-  // applySimpleLightbox();
+  }, '');
+  refs.galleryEl.insertAdjacentHTML("beforeend", markupString);
+  smoothScroll();
+ 
+  gallery.refresh();
+  applySimpleLightbox();
 }
 
 function isListOfItemsFinished() {
   if (totalItems - limOnPage * currentPage <= 0) {
     refs.loadMoreBtnEl.classList.add('hidden');
-    Notify.failure("We're sorry, but you've reached the end of search results.")
+    Notify.failure("We're sorry, but you've reached the end of search results.", {width: '400px', fontSize: '18px'})
   return
   }
  refs.loadMoreBtnEl.classList.remove('hidden');
 }
 
 function addTotalHitsInfo(totalItems) {
-  refs.galleryEl.innerHTML = `<p class="totalHits" >Hooray! We found ${totalItems} images.</p >`
+  refs.totalItemsEl.classList.remove('hidden')
+   refs.totalItemsEl.textContent = `Hooray! We found ${totalItems} images.`
 }
-  
-
-
- refs.loadMoreBtnEl.addEventListener('click', handleLoadMoreClick);
+ 
 function handleLoadMoreClick(event) {
   currentPage += 1;
   getFetchPicture(valueOfInput);
 }
 
-// function applySimpleLightbox() {
-//   let gallery = new SimpleLightbox('.gallery a');
-// gallery.on('show.simplelightbox', function () {
-// 	console.log('aaaa')
-// });
-// }
+
+function applySimpleLightbox() {
+ 
+  gallery.on('show.simplelightbox', function () {
+});
+  
+}
+
+function smoothScroll() {
+  const { height: cardHeight } = refs.galleryEl
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+}
