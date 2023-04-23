72dec1b99f7dab51b5df7b6c9f592bf72e33a4fd
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-04-24 00:00:57 +0100

    Update buttonToTop.js

--- src/js/buttonToTop.js
+++ src/js/buttonToTop.js
@@ -20,5 +20,5 @@ scrollFunction() {
     // or another option 
   // document.body.scrollTop = 0; // For Safari
   // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  //   };
+    };
 }
