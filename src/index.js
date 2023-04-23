bbfe432eb5b14e1ffe39855d890b5a06e9238bf2
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-04-23 23:57:56 +0100

    q

--- src/index.js
+++ src/index.js
@@ -129,23 +129,5 @@ function addTotalHitsInfo(totalItems) {
 
 
 
-// const buttonToTop = document.querySelector('.toTop');
-// console.log(buttonToTop)
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//    buttonToTop.style.display = "block";
-//   } else {
-//     buttonToTop.style.display = "none";
-//   }
-// }
-
-// buttonToTop.addEventListener('click', topFunction)
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
 
 
